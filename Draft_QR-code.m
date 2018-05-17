2991846f9ee52899ff770a28d3d3cf75947ed916
Rui de Sá <ruipedrodesa@gmail.com>
2018-05-17 18:00:13 +0100

    não mudei nada, testing matlab git

--- Draft_QR-code.m
+++ Draft_QR-code.m
@@ -2,12 +2,12 @@
 
 pioneer_init(sp); % wtf is sp???
 
-%while flag (true at√© chegar ao QR-code que diz que n√£o xD)
+%while flag (true atÈ chegar ao QR-code que diz que n„o xD)
 
-    % epa √© s√≥ meter QR-codes para virar √† direita nos cantos e um para virar √† esquerda na porta do lab
+    % epa È sÛ meter QR-codes para virar ‡ direita nos cantos e um para virar ‡ esquerda na porta do lab
 
     % Again, controlar o movimento do robot...
 
-% √© s√≥ isto, i guess.
+% È sÛ isto, i guess.
 
-% Este parece ser giro, tbh, prefiro este a hard-coded, √© mais flexivel.
+% Este parece ser giro, tbh, prefiro este a hard-coded, È mais flexivel.
